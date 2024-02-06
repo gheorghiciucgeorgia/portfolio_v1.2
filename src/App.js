c29ee419f7757eb9eeb0a64054e69d9650e8960a
gheorghiciucgeorgia <georgia-elena.gheorghiciuc@studen.tuiasi.ro>
2024-02-06 16:26:38 +0200

    logo added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './scss/App.scss';
 import MobileMenu from './components/MobileMenu';
+import logo from "./assets/logo/light logo.svg"
 function App() {
   return (
     <div className="App">
     <header className='header'>
       <div className='header-top'>
         <MobileMenu></MobileMenu>
+        <img className="logo" src={logo} alt="logo"/>
       </div>
       <div className='header-contents'>
 
